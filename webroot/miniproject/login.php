8c41be79d75ba180e70a841800b101dc020d113f
Qterion <60145661+Qterion@users.noreply.github.com>
2020-05-06 17:16:32 +0100

    changed

--- webroot/miniproject/login.php
+++ webroot/miniproject/login.php
@@ -1,8 +1,15 @@
 <?php 
 session_start();
 
-// connect to database
-$db = mysqli_connect('localhost', 'root', '', 'users');
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER");
+$dbpwd = getenv("DATABASE_PASSWORD");
+$dbname = getenv("DATABASE_NAME");
+$db = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($db->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+}
 
 // variable declaration
 $username = "";
